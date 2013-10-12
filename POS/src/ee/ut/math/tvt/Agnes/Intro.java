8da968dcac532f0683652df87377673825920441
Raido Uudel <raido.uudel@hotmail.com>
2013-10-12 21:52:57 +0300

    Klassid valmis ja ma panin mingi pildi ka isiklikku maitse järgi.

--- POS/src/ee/ut/math/tvt/Agnes/Intro.java
+++ POS/src/ee/ut/math/tvt/Agnes/Intro.java
@@ -3,10 +3,11 @@ package ee.ut.math.tvt.Agnes;
 import org.apache.log4j.Logger;
 
 public class Intro {
+	
 	private static final Logger logger = Logger.getLogger(Intro.class);
 	
 	public static void main(String[] args) {
-		
+		new IntroUI();
 	}
 
 }
--- POS/src/ee/ut/math/tvt/Agnes/IntroUI.java
+++ POS/src/ee/ut/math/tvt/Agnes/IntroUI.java
@@ -1,5 +1,79 @@
 package ee.ut.math.tvt.Agnes;
 
-public class IntroUI {
+import java.awt.BorderLayout;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+import javax.imageio.ImageIO;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class IntroUI {
+	
+	JFrame f = new JFrame("Agnes POS");
+	JPanel p = new JPanel();
+	JPanel pic = new JPanel();
+	
+	String teamName = "";
+	String teamLeader = "";
+	String leaderEmail = "";
+	String teamMembers = "";
+	String version = "";
+
+	JLabel img = new JLabel();
+	
+	Properties prop = new Properties();
+	
+	public IntroUI(){
+		
+		try {
+			prop.load(new FileInputStream(new File("conf/application.properties")));
+			teamName = prop.getProperty("team.name");
+			teamLeader = prop.getProperty("team.leader");
+			leaderEmail = prop.getProperty("team.leader.email");
+			teamMembers = prop.getProperty("team.members");
+			
+			prop.load(new FileInputStream(new File("conf/version.properties")));
+			version = prop.getProperty("build.number");
+		}
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		try {
+			img = new JLabel(new ImageIcon(ImageIO.read(new File("Agnes.jpg"))));
+		} catch (IOException e) {
+		}
+		
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		f.setLocationRelativeTo(null);
+		f.setLayout(new BorderLayout());
+		f.setResizable(false);
+		
+		p.setLayout(new BoxLayout(p, 1));
+		
+		p.add(new JLabel("Team name: " + teamName));
+		p.add(Box.createVerticalStrut(10));
+		p.add(new JLabel("Team leader: " + teamLeader));
+		p.add(Box.createVerticalStrut(10));
+		p.add(new JLabel("Team leader email: " + leaderEmail));
+		p.add(Box.createVerticalStrut(10));
+		p.add(new JLabel("Team members: " + teamMembers));
+		p.add(Box.createVerticalStrut(10));
+		p.add(new JLabel("Version: " + version));
+		
+		pic.add((img));
+		
+		f.add(p, BorderLayout.WEST);
+		f.add(pic, BorderLayout.EAST);
+		f.pack();
+	}
 }
