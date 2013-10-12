d1dc07ac0efcc591f3d4da5e363ebddd81c538b3
KL <KL@torn.lan>
2013-10-12 14:35:30 +0300

    Korrastus, versiooninumbrid, ressursside lokeerimine ehitamisel

--- POS/src/ee/ut/math/tvt/Agnes/Intro.java
+++ POS/src/ee/ut/math/tvt/Agnes/Intro.java
@@ -1,5 +1,12 @@
 package ee.ut.math.tvt.Agnes;
 
+import org.apache.log4j.Logger;
+
 public class Intro {
+	private static final Logger logger = Logger.getLogger(Intro.class);
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
